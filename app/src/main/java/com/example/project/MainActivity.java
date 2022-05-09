dff190ef758d227664083d420fe511f19067a74b
Pontus Kronenberg <a21ponkr@student.his.se>
2022-05-09 07:51:32 +0200

    moaaaaaaar

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,56 @@
 package com.example.project;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        button = (Button) findViewById(R.id.buttonOne);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+
+            }
+        });
+
+
+    }
+
+    public void onResume() {
+        super.onResume();
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
+        TextView t1;
+        t1 = findViewById(R.id.editThisText);
+        t1.setText(sp.getString("TextEdit","idk"));
+
+
+        //t1.setText(name);
+
+    }
+
+
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 
 }
